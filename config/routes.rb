4399693efa3d508c382203196f02396139398c7a
Nick Benoit <nick.benoit@atomicjolt.com>
2022-08-27 08:41:58 -0600

    Add deployment controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,8 @@ AtomicAdmin::Engine.routes.draw do
     # namespace :lti do 
   resources :atomic_lti_platform
   resources :atomic_lti_install
+  resources :atomic_lti_deployment
     # end
     # TODO 
-    # platforms
-    # installs
-    # deployments
     # strategies
 end
