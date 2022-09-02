107e70d8e39b93ec6ae2356fdfad8e07c3920a10
Nick Benoit <nick.benoit@atomicjolt.com>
2022-09-02 09:47:43 -0600

    Fix finding platform guid by id

--- app/controllers/atomic_admin/atomic_tenant_platform_guid_strategy_controller.rb
+++ app/controllers/atomic_admin/atomic_tenant_platform_guid_strategy_controller.rb
@@ -5,33 +5,39 @@ module AtomicAdmin
     end
 
     def find_pinned_platform_guid
-      AtomicTenant::PinnedPlatformGuid.find_by(id: params[:id])
+      AtomicTenant::PinnedPlatformGuid.find(params[:id])
     end
 
     def index 
-      page = AtomicTenant::PinnedPlatformGuid.all.all.order(:id).paginate(page: params[:page], per_page: 30)
-      render json: page
+      page = AtomicTenant::PinnedPlatformGuid.all.order(:id).paginate(page: params[:page], per_page: 30)
+      render json: {
+        pinned_platform_guids: page,
+        page: params[:page],
+        total_pages: page.total_pages
+      }
     end
 
     def create
-      AtomicLti::Platform.create!(pinned_platform_guid_params)
+      result = AtomicTenant::PinnedPlatformGuid.create!(pinned_platform_guid_params)
+      render json: { pinned_platform_guid: result }
     end
 
     def show
-      platform = find_pinned_platform_guid
-      render json: platform
+      pinned_platform_guid = find_pinned_platform_guid
+      render json: {pinned_platform_guid: pinned_platform_guid}
     end
-    
-    # def update
-    #   platform = find_pinned_platform_guid
-    #   result = platform.update!(pinned_platform_guid_params)
-    #   render json: result
-    # end
+
+    def update
+      pinned_platform_guid = find_pinned_platform_guid
+      pinned_platform_guid.update!(pinned_platform_guid_params)
+
+      render json: {pinned_platform_guid: find_pinned_platform_guid}
+    end
 
     def destroy
-      platform = find_pinned_platform_guid
-      platform.destroy
-      render json: platform
+      pinned_platform_guid = find_pinned_platform_guid
+      pinned_platform_guid.destroy
+      render json: { pinned_platform_guid: pinned_platform_guid }
     end
   end
 end
